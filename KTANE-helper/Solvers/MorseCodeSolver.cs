24cd97cca7014963a83faf56dddb861c97f0e963
JochemBloem <45417884+JochemBloem@users.noreply.github.com>
2022-08-23 00:26:18 +0200

    Implemented the morse code solver

--- KTANE-helper/Solvers/MorseCodeSolver.cs
+++ KTANE-helper/Solvers/MorseCodeSolver.cs
@@ -3,34 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static KTANE_helper.IOHandler;
 
 namespace KTANE_helper.Solvers
 {
     internal class MorseCodeSolver : Solvable<MorseCodeSolver>
     {
-        private readonly Dictionary<string, double> possibilities = new()
+        private List<string> wordOptions;
+
+        private readonly Dictionary<string, string> morseCode = new()
         {
-            { "halls", 3.515 },
-            { "shell", 3.505 },
-            { "slick", 3.522 },
-            { "trick", 3.532 },
-            { "boces", 3.535 },
-            { "leaks", 3.542 },
-            { "strobe", 3.545 },
-            { "bistro", 3.552 },
-            { "flick", 3.555 },
-            { "bombs", 3.565 },
-            { "break", 3.572 },
-            { "brick", 3.575 },
-            { "steak", 3.582 },
-            { "sting", 3.592 },
-            { "vector", 3.595 },
-            { "beats", 3.600 },
+            { ".-"  , "a" },
+            { "-...", "b" },
+            { "-.-.", "c" },
+            { "."   , "e" },
+            { "..-.", "f" },
+            { "--." , "g" },
+            { "....", "h" },
+            { ".."  , "i" },
+            { "-.-" , "k" },
+            { ".-..", "l" },
+            { "--"  , "m" },
+            { "-."  , "n" },
+            { "---" , "o" },
+            { ".-." , "r" },
+            { "..." , "s" },
+            { "-"   , "t" },
+            { "...-", "v" },
+            { "-..-", "x" },
         };
 
-        internal override void Solve(BombKnowledge bk)
+        private readonly Dictionary<string, double> words = new()
         {
+            { "beats", 3.600 },
+            { "bistro", 3.552 },
+            { "bombs", 3.565 },
+            { "boxes", 3.535 },
+            { "break", 3.572 },
+            { "brick", 3.575 },
+            { "flick", 3.555 },
+            { "halls", 3.515 },
+            { "leaks", 3.542 },
+            { "shell", 3.505 },
+            { "slick", 3.522 },
+            { "steak", 3.582 },
+            { "sting", 3.592 },
+            { "strobe", 3.545 },
+            { "trick", 3.532 },
+            { "vector", 3.595 }
+        };
+
+        
+        internal override void Solve(BombKnowledge _)
+        {
+            wordOptions = words.Keys.ToList();
+
+            Show("Use . for shorts and - or , for longs.");
+            string answer;
+            while (!ProcessLetter(Query("What is a letter you see?"), out answer));
+
+            if (answer is not null) Show($"The correct frequency is {words[answer]} MHz");
+            else
+            {
+                Show("you f'ed up!");
+                Solve(_);
+            }
+        }
+
+        private bool ProcessLetter(string letter, out string word)
+        {
+            word = null;
+
+            letter = letter.Replace(',', '-');
+            if (!morseCode.Keys.Contains(letter)) return false;
+
+            wordOptions = wordOptions.Where(word => word.Contains(morseCode[letter])).ToList();
             
+            if (!wordOptions.Any()) return true;
+
+            word = wordOptions[0];
+
+            return wordOptions.Count == 1;
         }
     }
 }
