78abea5ab18df1e1f062f4bb2c0e48d6823a9d10
Loïse <izakhaak@gmail.com>
2025-01-12 18:30:33 +0100

    #24 split up IOHandler logic

---
+++ KTANE-helper/IOHandler/CLIHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace KTANE_helper.IOHandler;
+
+public class CLIHandler : IOHandler
+{
+    public override void Show(string message) => Console.Write(message);
+    public override void ShowLine(string message) => Console.WriteLine(message);
+    public override string ReadLine() => Console.ReadLine();
+}
