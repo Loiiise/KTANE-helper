f4886c112903ab91a244c279020cfd23ef9c5237
Loïse <izakhaak@gmail.com>
2025-01-12 18:54:29 +0100

    #24 fixed bombknowledee

--- KTANE-helper/BombKnowledge.cs
+++ KTANE-helper/BombKnowledge.cs
@@ -1,45 +1,50 @@
-﻿using System.Collections.Generic;
-using static KTANE_helper.IOHandler;
+﻿using KTANE_helper.IOHandler;
+using System.Collections.Generic;
 
-namespace KTANE_helper
+namespace KTANE_helper;
+
+internal class BombKnowledge
 {
-    internal class BombKnowledge
+    internal BombKnowledge(IIOHandler ioHandler)
     {
-        private int? batteries;
-
-        internal int Batteries() => batteries.HasValue
-            ? batteries.Value
-            : (batteries = IntQuery("How many batteries are present on the bomb?")).Value;
-
-
-        private bool? serialNumberLastDigitEven;
-
-        internal bool SerialNumberLastDigitEven() => serialNumberLastDigitEven.HasValue
-            ? serialNumberLastDigitEven.Value
-            : (serialNumberLastDigitEven = IntQuery("What is the last digit of the serial number?") % 2 == 0).Value;
-        internal bool SerialNumberLastDigitOdd() => !this.SerialNumberLastDigitEven();
-
-        private List<string> litIndicators = new();
-        internal bool LitIndicator(string indicator)
-        {
-            indicator = indicator.ToUpper();
-            if (litIndicators.Contains(indicator)) return true;
-
-            bool present = Ask($"Lit indicator \"{indicator}\"?");
-            if (present) litIndicators.Add(indicator);
-
-            return present;
-        }
-
-        private bool? serialNumberContainsVowel;
-        internal bool SerialNumberContainsVowel() => serialNumberContainsVowel.HasValue
-            ? serialNumberContainsVowel.Value
-            : (serialNumberContainsVowel = Ask("Does the serial number contain a vowel?")).Value;
-
-        private bool? hasParallelPort;
-        internal bool HasParallelPort() => hasParallelPort.HasValue
-            ? hasParallelPort.Value
-            : (hasParallelPort = Ask("Does the bomb have a parallel port?")).Value;
-
+        _ioHandler = ioHandler;
     }
+
+    private int? batteries;
+
+    internal int Batteries() => batteries.HasValue
+        ? batteries.Value
+        : (batteries = _ioHandler.IntQuery("How many batteries are present on the bomb?")).Value;
+
+    private bool? serialNumberLastDigitEven;
+
+    internal bool SerialNumberLastDigitEven() => serialNumberLastDigitEven.HasValue
+        ? serialNumberLastDigitEven.Value
+        : (serialNumberLastDigitEven = _ioHandler.IntQuery("What is the last digit of the serial number?") % 2 == 0).Value;
+    internal bool SerialNumberLastDigitOdd() => !this.SerialNumberLastDigitEven();
+
+    private List<string> litIndicators = new();
+    internal bool LitIndicator(string indicator)
+    {
+        indicator = indicator.ToUpper();
+        if (litIndicators.Contains(indicator)) return true;
+
+        bool present = _ioHandler.Ask($"Lit indicator \"{indicator}\"?");
+        if (present) litIndicators.Add(indicator);
+
+        return present;
+    }
+
+    private bool? serialNumberContainsVowel;
+    internal bool SerialNumberContainsVowel() => serialNumberContainsVowel.HasValue
+        ? serialNumberContainsVowel.Value
+        : (serialNumberContainsVowel = _ioHandler.Ask("Does the serial number contain a vowel?")).Value;
+
+    private bool? hasParallelPort;
+
+    internal bool HasParallelPort() => hasParallelPort.HasValue
+        ? hasParallelPort.Value
+        : (hasParallelPort = _ioHandler.Ask("Does the bomb have a parallel port?")).Value;
+
+    private readonly IIOHandler _ioHandler;
 }
--- KTANE-helper/Game.cs
+++ KTANE-helper/Game.cs
@@ -1,15 +1,122 @@
 using KTANE_helper.IOHandler;
 using KTANE_helper.NeedyModules;
 using KTANE_helper.Solvers;
+using System.Collections.Generic;
 
 namespace KTANE_helper;
 
 class Game
 {
-    BombKnowledge bk = new();
-    private List<string> validInputs = new();
-    Dictionary<string, InputToken> tokenMap = new();
-    readonly Dictionary<InputToken, List<string>> invocations = new()
+    public Game(IIOHandler ioHandler)
+    {
+        _ioHandler = ioHandler;
+        _bombKnowledge = new(_ioHandler);
+
+        // Flip the script
+        foreach ((var token, var values) in _invocations)
+        {
+            _validInputs.AddRange(values);
+            foreach (var value in values)
+                _tokenMap[value] = token;
+        }
+
+        Play();
+    }
+
+    private void Play()
+    {
+        InputToken token;
+
+        while ((token = NextToken()) is not InputToken.Quit)
+        {
+            if (token == InputToken.NextBomb)
+            {
+                _ioHandler.ShowLine("I hear trouble coming... Over and over again");
+                _bombKnowledge = new(_ioHandler);
+                continue;
+            }
+
+            SolvePuzzle(token);
+        }
+    }
+
+
+    private InputToken NextToken() => _tokenMap[_ioHandler.Query("What do you want to do next?", _validInputs)];
+
+    private void SolvePuzzle(InputToken puzzle)
+    {
+        _ioHandler.PromptScopeUp(puzzle.ToString().ToLower());
+
+        switch (puzzle)
+        {
+            case InputToken.Wires:
+                WireSolver.GetInstance(_ioHandler).Solve(_bombKnowledge);
+                break;
+            case InputToken.Button:
+                ButtonSolver.GetInstance(_ioHandler).Solve(_bombKnowledge);
+                break;
+            case InputToken.Keypad:
+                KeypadSolver.GetInstance(_ioHandler).Solve(_bombKnowledge);
+                break;
+            case InputToken.SimonSays:
+                SimonSaysSolver.GetInstance(_ioHandler).Solve(_bombKnowledge);
+                break;
+            case InputToken.WhosOnFirst:
+                WhosOnFirstSolver.GetInstance(_ioHandler).Solve(_bombKnowledge);
+                break;
+            case InputToken.Memory:
+                MemorySolver.GetInstance(_ioHandler).Solve(_bombKnowledge);
+                break;
+            case InputToken.MorseCode:
+                MorseCodeSolver.GetInstance(_ioHandler).Solve(_bombKnowledge);
+                break;
+            case InputToken.ComplicatedWires:
+                ComplicatedWiresSolver.GetInstance(_ioHandler).Solve(_bombKnowledge);
+                break;
+            case InputToken.WireSequences:
+                WireSequenceSolver.GetInstance(_ioHandler).Solve(_bombKnowledge);
+                break;
+            case InputToken.Maze:
+                MazeSolver.GetInstance(_ioHandler).Solve(_bombKnowledge);
+                break;
+            case InputToken.Password:
+                PasswordSolver.GetInstance(_ioHandler).Solve(_bombKnowledge);
+                break;
+            case InputToken.NeedyKnob:
+                NeedyKnob.GetInstance(_ioHandler).Solve(_bombKnowledge);
+                break;
+            default:
+                _ioHandler.ShowLine("This solver is not implemented. You're on your own!");
+                break;
+        }
+
+        _ioHandler.PromptScopeDown();
+    }
+
+    private enum InputToken
+    {
+        Quit,
+        NextBomb,
+        Wires,
+        Button,
+        Keypad,
+        SimonSays,
+        WhosOnFirst,
+        Memory,
+        MorseCode,
+        ComplicatedWires,
+        WireSequences,
+        Maze,
+        Password,
+        NeedyKnob,
+    }
+
+    private BombKnowledge _bombKnowledge;
+    private List<string> _validInputs = new();
+    private Dictionary<string, InputToken> _tokenMap = new();
+
+    private readonly IIOHandler _ioHandler;
+    private readonly Dictionary<InputToken, List<string>> _invocations = new()
     {
         {
             InputToken.Quit,
@@ -39,108 +146,4 @@ class Game
         { InputToken.Password, new() { "p", "pw", "password" } },
         { InputToken.NeedyKnob, new() { "nn", "needy", "knobs", "needyknob" } },
     };
-
-    public Game(IIOHandler ioHandler)
-    {
-        _ioHandler = ioHandler;
-        // Flip the script
-        foreach ((var token, var values) in invocations)
-        {
-            validInputs.AddRange(values);
-            foreach (var value in values)
-                tokenMap[value] = token;
-        }
-
-        Play();
-    }
-
-    private void Play()
-    {
-        InputToken token;
-
-        while ((token = NextToken()) is not InputToken.Quit)
-        {
-            if (token == InputToken.NextBomb)
-            {
-                _ioHandler.ShowLine("I hear trouble coming... Over and over again");
-                bk = new();
-                continue;
-            }
-
-            SolvePuzzle(token);
-        }
-    }
-
-
-    private InputToken NextToken() => tokenMap[_ioHandler.Query("What do you want to do next?", validInputs)];
-
-    private void SolvePuzzle(InputToken puzzle)
-    {
-        _ioHandler.PromptScopeUp(puzzle.ToString().ToLower());
-
-        switch (puzzle)
-        {
-            case InputToken.Wires:
-                WireSolver.GetInstance(_ioHandler).Solve(bk);
-                break;
-            case InputToken.Button:
-                ButtonSolver.GetInstance(_ioHandler).Solve(bk);
-                break;
-            case InputToken.Keypad:
-                KeypadSolver.GetInstance(_ioHandler).Solve(bk);
-                break;
-            case InputToken.SimonSays:
-                SimonSaysSolver.GetInstance(_ioHandler).Solve(bk);
-                break;
-            case InputToken.WhosOnFirst:
-                WhosOnFirstSolver.GetInstance(_ioHandler).Solve(bk);
-                break;
-            case InputToken.Memory:
-                MemorySolver.GetInstance(_ioHandler).Solve(bk);
-                break;
-            case InputToken.MorseCode:
-                MorseCodeSolver.GetInstance(_ioHandler).Solve(bk);
-                break;
-            case InputToken.ComplicatedWires:
-                ComplicatedWiresSolver.GetInstance(_ioHandler).Solve(bk);
-                break;
-            case InputToken.WireSequences:
-                WireSequenceSolver.GetInstance(_ioHandler).Solve(bk);
-                break;
-            case InputToken.Maze:
-                MazeSolver.GetInstance(_ioHandler).Solve(bk);
-                break;
-            case InputToken.Password:
-                PasswordSolver.GetInstance(_ioHandler).Solve(bk);
-                break;
-            case InputToken.NeedyKnob:
-                NeedyKnob.GetInstance(_ioHandler).Solve(bk);
-                break;
-            default:
-                _ioHandler.ShowLine("This solver is not implemented. You're on your own!");
-                break;
-        }
-
-        _ioHandler.PromptScopeDown();
-    }
-
-    private enum InputToken
-    {
-        Quit,
-        NextBomb,
-        Wires,
-        Button,
-        Keypad,
-        SimonSays,
-        WhosOnFirst,
-        Memory,
-        MorseCode,
-        ComplicatedWires,
-        WireSequences,
-        Maze,
-        Password,
-        NeedyKnob,
-    }
-
-    private readonly IIOHandler _ioHandler;
 }
