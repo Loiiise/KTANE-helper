93814453369bcae21315e3a5cfbda821561a2f68
JochemBloem <45417884+JochemBloem@users.noreply.github.com>
2025-01-15 22:11:21 +0100

    Removed empty code blocks from Answer.cs

--- KTANE-helper/KTANE-helper.Logic/IO/IOToken/Answer.cs
+++ KTANE-helper/KTANE-helper.Logic/IO/IOToken/Answer.cs
@@ -13,22 +13,22 @@ public abstract record Answer<T> : IOToken
 /// <param name="ReleaseOrHold">Action to perform</param>
 /// <param name="When">The second on which it should be released in case of <a cref="ButtonReleaseOrHold.ReleaseWhen"/></param>
 public sealed record ButtonAnswerValue(ButtonReleaseOrHold ReleaseOrHold, int? When);
-public sealed record ButtonAnswer : Answer<ButtonAnswerValue> { }
+public sealed record ButtonAnswer : Answer<ButtonAnswerValue>;
 
 /// <summary>
 /// Whether or not to cut the wire.
 /// </summary>
-public sealed record ComplicatedWiresAnswer : Answer<bool> { }
+public sealed record ComplicatedWiresAnswer : Answer<bool>;
 
 /// <summary>
 /// The sequence in which the <a cref="KeypadSymbol"/>s should be pressed.
 /// </summary>
-public sealed record KeypadAnswer : Answer<KeypadSymbol[]> { }
+public sealed record KeypadAnswer : Answer<KeypadSymbol[]>;
 
 /// <summary>
 /// A sequence of steps for the player to reach the destination.
 /// </summary>
-public sealed record MazeAnswer : Answer<MazeDirection[]> { }
+public sealed record MazeAnswer : Answer<MazeDirection[]>; 
 
 /// <summary>
 /// The x'th position or label which should be pressed. 
@@ -36,35 +36,35 @@ public sealed record MazeAnswer : Answer<MazeDirection[]> { }
 /// <param name="PositionOrLabel">Self explanatory</param>
 /// <param name="Value">The x'th postition or label to press, dependant on <a cref="MemoryPositionOrLabel.ReleaseWhen"/></param>
 public sealed record MemoryAnswerValue(MemoryPositionOrLabel PositionOrLabel, int Value);
-public sealed record MemoryAnswer : Answer<MemoryAnswerValue> { }
+public sealed record MemoryAnswer : Answer<MemoryAnswerValue>;
 
 /// <summary>
 /// The frequency.
 /// </summary>
-public sealed record MorseCodeAnswer : Answer<double> { }
+public sealed record MorseCodeAnswer : Answer<double>;
 
 /// <summary>
 /// The password.
 /// </summary>
-public sealed record PasswordAnswer : Answer<string> { }
+public sealed record PasswordAnswer : Answer<string>;
 
 /// <summary>
 /// The sequence of buttons which should be pressed.
 /// </summary>
-public sealed record SimonSaysAnswer : Answer<SimonSaysColour[]> { }
+public sealed record SimonSaysAnswer : Answer<SimonSaysColour[]>;
 
 /// <summary>
 /// The order of occurrence. First one that is visible should be cut.
 /// </summary>
-public sealed record WhosOnFirstAnswer : Answer<string[]> { }
+public sealed record WhosOnFirstAnswer : Answer<string[]>;
 
 /// <summary>
 /// Which wires should be cut.
 /// </summary>
 public sealed record WireSequenceAnswerValue(WireSequenceAnswerState WireSequenceAnswerState, int[] WiresToCutIfCustom);
-public sealed record WireSequenceAnswer : Answer<WireSequenceAnswerValue> { }
+public sealed record WireSequenceAnswer : Answer<WireSequenceAnswerValue>;
 
 /// <summary>
 /// The x'th wire that should be cut.
 /// </summary>
-public sealed record WireAnswer : Answer<int> { }
+public sealed record WireAnswer : Answer<int>;
